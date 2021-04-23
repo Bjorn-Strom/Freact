591765d15dde033bf3f10c917f2d304cb38fb2c3
Bjorn-Strom <bjorn.ivar.strom@bekk.no>
2021-04-23 12:32:32 +0200

    Hacking

--- app.fs
+++ app.fs
@@ -3,7 +3,6 @@ module Freact =
     open Browser.Dom
 
     // Todo: Get state working
-
     type HtmlTag =
         | Div
         | Str of string
@@ -14,7 +13,11 @@ module Freact =
           Children: Html list
         }
     and Html =
-        HtmlTag * HtmlAttributes
+        | RegularHtml of HtmlTag * HtmlAttributes
+        | Component of (obj list -> Html) * (unit -> Html)
+
+    let ( </> ) (comp: 'a -> Html) (props: 'a) =
+        Component (comp, fun () -> comp props)
 
     type IDomNode = interface end
     type TextNode =
@@ -31,30 +34,43 @@ module Freact =
                 OnClick = None 
                 Children = children
               })
+        |> RegularHtml
 
     let str str =
         (Str str, { ClassName = None
                     OnClick = None 
                     Children = []
                   })
+        |> RegularHtml
 
     let className className f children =
-        let (tag, rec') = f children
-        (tag, { rec' with 
-                    ClassName = Some className 
-                    })
+        match f children with
+        | RegularHtml (tag, rec') -> 
+            (tag, { rec' with 
+                        ClassName = Some className 
+                  })
+            |> RegularHtml
+        | a -> a
 
     let onClick onClick f children =
-        let (tag, rec') = f children
-        (tag, { rec' with 
-                    OnClick = Some onClick
-                        })
+        match f children with
+        | RegularHtml (tag, rec') ->
+            (tag, { rec' with 
+                        OnClick = Some onClick
+                  })
+            |> RegularHtml
+        | a -> a
+
+    let mutable state: Map<(obj list -> Html), string> = Map.empty
 
     let useState s =
         let mutable state = s
         (s, fun s' -> state <- s')
+
+    let Kom [] =
+        div <| [ str "Chicks" ]
             
-    let myView: Html =
+    let MyView []: Html =
         let (n, setN) = useState 0
         div
         |> className "lol"
@@ -63,39 +79,48 @@ module Freact =
             div
             |> className "Duck"
             <| [ str "Carts" ]
+
+            if n > 3 then
+                Kom </> []
+
+            if false then
+                div []
+
             div
             |> onClick (fun () -> printf "you" |> ignore)
             <| [ str "Sticks"]
             div
-            <| [ str (sprintf "%A" n)]
+            <| [ str (sprintf "%A" n) ]
         ]
 
 
     let rootContainer = document.getElementById "app"
 
     let rec render element (container: Browser.Types.HTMLElement) =
-        let (tag, attributes: HtmlAttributes) = element
-        let newDomElement =
-            match tag with
-            | Str s ->
-                document.createTextNode s |> TextNode :> IDomNode
-            | Div ->
-                document.createElement "div" |> ElementNode :> IDomNode
+        match element with
+        | RegularHtml (tag, attributes: HtmlAttributes) ->
+            let newDomElement =
+                match tag with
+                | Str s ->
+                    document.createTextNode s |> TextNode :> IDomNode
+                | Div ->
+                    document.createElement "div" |> ElementNode :> IDomNode
 
-        match newDomElement with
-        | :? TextNode as t ->
-            container.appendChild <| unboxTextNode t |> ignore
-        | :? ElementNode as e ->
-            let e = unboxElementNode e
-            if attributes.OnClick.IsSome then
-                e.addEventListener("click", fun _ -> 
-                                                attributes.OnClick.Value ()
-                                                // Hack hack hack
-                                                rootContainer.innerHTML <- ""
-                                                render myView rootContainer)
-            List.iter (fun x -> render x e) attributes.Children
-            container.appendChild e |> ignore
+            match newDomElement with
+            | :? TextNode as t ->
+                container.appendChild <| unboxTextNode t |> ignore
+            | :? ElementNode as e ->
+                let e = unboxElementNode e
+                if attributes.OnClick.IsSome then
+                    e.addEventListener("click", fun _ -> 
+                                                    attributes.OnClick.Value ()
+                                                    // Hack hack hack
+                                                    rootContainer.innerHTML <- ""
+                                                    render (MyView </> []) rootContainer)
+                List.iter (fun x -> render x e) attributes.Children
+                container.appendChild e |> ignore
+        | Component (f, html) ->
+            render (html ()) container
         
 
-
-    render myView rootContainer
+    render (MyView </> []) rootContainer
--- public/app.js
+++ public/app.js
@@ -1,6 +1,9 @@
 import { MatchFailureException, Record, Union } from "./.fable/fable-library.3.1.15/Types.js";
-import { class_type, record_type, list_type, tuple_type, lambda_type, unit_type, option_type, union_type, string_type } from "./.fable/fable-library.3.1.15/Reflection.js";
-import { iterate, ofArray, singleton, empty } from "./.fable/fable-library.3.1.15/List.js";
+import { class_type, obj_type, record_type, list_type, lambda_type, unit_type, option_type, union_type, string_type } from "./.fable/fable-library.3.1.15/Reflection.js";
+import { iterate, singleton, isEmpty, empty } from "./.fable/fable-library.3.1.15/List.js";
+import { createAtom } from "./.fable/fable-library.3.1.15/Util.js";
+import { empty as empty_1 } from "./.fable/fable-library.3.1.15/Map.js";
+import { empty as empty_2, singleton as singleton_1, append, delay, toList } from "./.fable/fable-library.3.1.15/Seq.js";
 import { toText, printf, toConsole } from "./.fable/fable-library.3.1.15/String.js";
 import { value as value_2 } from "./.fable/fable-library.3.1.15/Option.js";
 
@@ -29,7 +32,26 @@ export class HtmlAttributes extends Record {
 }
 
 export function HtmlAttributes$reflection() {
-    return record_type("Freact.Freact.HtmlAttributes", [], HtmlAttributes, () => [["ClassName", option_type(string_type)], ["OnClick", option_type(lambda_type(unit_type, unit_type))], ["Children", list_type(tuple_type(HtmlTag$reflection(), HtmlAttributes$reflection()))]]);
+    return record_type("Freact.Freact.HtmlAttributes", [], HtmlAttributes, () => [["ClassName", option_type(string_type)], ["OnClick", option_type(lambda_type(unit_type, unit_type))], ["Children", list_type(Html$reflection())]]);
+}
+
+export class Html extends Union {
+    constructor(tag, ...fields) {
+        super();
+        this.tag = (tag | 0);
+        this.fields = fields;
+    }
+    cases() {
+        return ["RegularHtml", "Component"];
+    }
+}
+
+export function Html$reflection() {
+    return union_type("Freact.Freact.Html", [], Html, () => [[["Item1", HtmlTag$reflection()], ["Item2", HtmlAttributes$reflection()]], [["Item1", lambda_type(list_type(obj_type), Html$reflection())], ["Item2", lambda_type(unit_type, Html$reflection())]]]);
+}
+
+export function op_LessDivideGreater(comp, props) {
+    return new Html(1, comp, () => comp(props));
 }
 
 export class TextNode extends Union {
@@ -73,82 +95,127 @@ function unboxElementNode(_arg1) {
 }
 
 export function div(children) {
-    return [new HtmlTag(0), new HtmlAttributes(void 0, void 0, children)];
+    const tupledArg = [new HtmlTag(0), new HtmlAttributes(void 0, void 0, children)];
+    return new Html(0, tupledArg[0], tupledArg[1]);
 }
 
 export function str(str_1) {
-    return [new HtmlTag(1, str_1), new HtmlAttributes(void 0, void 0, empty())];
+    const tupledArg = [new HtmlTag(1, str_1), new HtmlAttributes(void 0, void 0, empty())];
+    return new Html(0, tupledArg[0], tupledArg[1]);
 }
 
 export function className(className_1, f, children) {
-    const patternInput = f(children);
-    const tag = patternInput[0];
-    const rec$0027 = patternInput[1];
-    return [tag, new HtmlAttributes(className_1, rec$0027.OnClick, rec$0027.Children)];
+    const matchValue = f(children);
+    if (matchValue.tag === 0) {
+        const tag = matchValue.fields[0];
+        const rec$0027 = matchValue.fields[1];
+        const tupledArg = [tag, new HtmlAttributes(className_1, rec$0027.OnClick, rec$0027.Children)];
+        return new Html(0, tupledArg[0], tupledArg[1]);
+    }
+    else {
+        const a = matchValue;
+        return a;
+    }
 }
 
 export function onClick(onClick_1, f, children) {
-    const patternInput = f(children);
-    const tag = patternInput[0];
-    const rec$0027 = patternInput[1];
-    return [tag, new HtmlAttributes(rec$0027.ClassName, onClick_1, rec$0027.Children)];
+    const matchValue = f(children);
+    if (matchValue.tag === 0) {
+        const tag = matchValue.fields[0];
+        const rec$0027 = matchValue.fields[1];
+        const tupledArg = [tag, new HtmlAttributes(rec$0027.ClassName, onClick_1, rec$0027.Children)];
+        return new Html(0, tupledArg[0], tupledArg[1]);
+    }
+    else {
+        const a = matchValue;
+        return a;
+    }
 }
 
+export let state = createAtom(empty_1());
+
 export function useState(s) {
-    let state = s;
+    let state_1 = s;
     return [s, (s$0027) => {
-        state = s$0027;
+        state_1 = s$0027;
     }];
 }
 
-export const myView = (() => {
-    const patternInput = useState(0);
-    const setN = patternInput[1];
-    const n = patternInput[0] | 0;
-    return onClick(() => {
-        setN(n + 1);
-    }, (children_1) => className("lol", (children) => div(children), children_1), ofArray([className("Duck", (children_3) => div(children_3), singleton(str("Carts"))), onClick(() => {
-        const value = toConsole(printf("you"));
-    }, (children_5) => div(children_5), singleton(str("Sticks"))), div(singleton(str(toText(printf("%A"))(n))))]));
-})();
-
-export const rootContainer = document.getElementById("app");
-
-export function render(element_0, element_1, container) {
-    const element = [element_0, element_1];
-    const tag = element[0];
-    const attributes = element[1];
-    let newDomElement;
-    if (tag.tag === 0) {
-        newDomElement = (new ElementNode(0, document.createElement("div")));
+export function Kom(_arg1) {
+    if (isEmpty(_arg1)) {
+        return div(singleton(str("Chicks")));
     }
     else {
-        const s = tag.fields[0];
-        newDomElement = (new TextNode(0, document.createTextNode(s)));
-    }
-    if (newDomElement instanceof TextNode) {
-        const t = newDomElement;
-        void container.appendChild(unboxTextNode(t));
-    }
-    else if (newDomElement instanceof ElementNode) {
-        const e = newDomElement;
-        const e_1 = unboxElementNode(e);
-        if (attributes.OnClick != null) {
-            e_1.addEventListener("click", (_arg1) => {
-                value_2(attributes.OnClick)();
-                rootContainer.innerHTML = "";
-                render(myView[0], myView[1], rootContainer);
-            });
-        }
-        iterate((x) => {
-            render(x[0], x[1], e_1);
-        }, attributes.Children);
-        void container.appendChild(e_1);
-    }
-    else {
-        throw (new MatchFailureException("/Users/bjornivarstrom/Documents/functional/Felm/app.fs", 85, 14));
+        throw (new Error("Match failure"));
     }
 }
 
-render(myView[0], myView[1], rootContainer);
+export function MyView(_arg1) {
+    if (isEmpty(_arg1)) {
+        const patternInput = useState(0);
+        const setN = patternInput[1];
+        const n = patternInput[0] | 0;
+        return onClick(() => {
+            setN(n + 1);
+        }, (children_1) => className("lol", (children) => div(children), children_1), toList(delay(() => append(singleton_1(className("Duck", (children_3) => div(children_3), singleton(str("Carts")))), delay(() => append((n > 3) ? singleton_1(op_LessDivideGreater((arg00$0040) => Kom(arg00$0040), empty())) : empty_2(), delay(() => append(empty_2(), delay(() => append(singleton_1(onClick(() => {
+            const value = toConsole(printf("you"));
+        }, (children_5) => div(children_5), singleton(str("Sticks")))), delay(() => singleton_1(div(singleton(str(toText(printf("%A"))(n))))))))))))))));
+    }
+    else {
+        throw (new Error("Match failure"));
+    }
+}
+
+export const rootContainer = document.getElementById("app");
+
+export function render(element_mut, container_mut) {
+    render:
+    while (true) {
+        const element = element_mut, container = container_mut;
+        if (element.tag === 1) {
+            const html = element.fields[1];
+            const f = element.fields[0];
+            element_mut = html();
+            container_mut = container;
+            continue render;
+        }
+        else {
+            const tag = element.fields[0];
+            const attributes = element.fields[1];
+            let newDomElement;
+            if (tag.tag === 0) {
+                newDomElement = (new ElementNode(0, document.createElement("div")));
+            }
+            else {
+                const s = tag.fields[0];
+                newDomElement = (new TextNode(0, document.createTextNode(s)));
+            }
+            if (newDomElement instanceof TextNode) {
+                const t = newDomElement;
+                void container.appendChild(unboxTextNode(t));
+            }
+            else if (newDomElement instanceof ElementNode) {
+                const e = newDomElement;
+                const e_1 = unboxElementNode(e);
+                if (attributes.OnClick != null) {
+                    e_1.addEventListener("click", (_arg1) => {
+                        value_2(attributes.OnClick)();
+                        rootContainer.innerHTML = "";
+                        render(op_LessDivideGreater((arg00$0040) => MyView(arg00$0040), empty()), rootContainer);
+                    });
+                }
+                iterate((x) => {
+                    render(x, e_1);
+                }, attributes.Children);
+                void container.appendChild(e_1);
+            }
+            else {
+                throw (new MatchFailureException("/Users/bjornivarstrom/Documents/functional/Felm/app.fs", 109, 18));
+            }
+        }
+        break;
+    }
+}
+
+render(op_LessDivideGreater((arg00$0040) => MyView(arg00$0040), empty()), rootContainer);
 
