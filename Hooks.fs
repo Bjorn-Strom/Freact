9987ab2065cfc32bfc5dba4420f4df9aa3cdb3d1
Bjorn-Strom <bjorn.ivar.strom@bekk.no>
2021-09-17 15:23:25 +0200

    Hook with resize array

--- Hooks.fs
+++ Hooks.fs
@@ -8,19 +8,18 @@ module HookInternals =
 
 [<RequireQualifiedAccess>]
 module Hooks =
-    type Hooks =
-        static member useState<'t> (initialValue: 't) =
-            if HookInternals.hooks.[HookInternals.currentHook] = null then
-                HookInternals.hooks.[HookInternals.currentHook] <- initialValue :> obj
+    let useState<'t> (initialValue: 't) =
+        if HookInternals.hooks.[HookInternals.currentHook] = null then
+            HookInternals.hooks.[HookInternals.currentHook] <- initialValue :> obj
 
-            let setStateHookIndex = HookInternals.currentHook
+        let setStateHookIndex = HookInternals.currentHook
 
-            let setState (newState: 't) =
-                // her må vi sjekke om det er en funksjon
-                HookInternals.hooks.[setStateHookIndex] <- newState :> obj
-                HookInternals.reRender()
+        let setState (newState: 't) =
+            // her må vi sjekke om det er en funksjon
+            HookInternals.hooks.[setStateHookIndex] <- newState :> obj
+            HookInternals.reRender()
 
-            let currentState = (box HookInternals.hooks.[setStateHookIndex]) :?> 't
+        let currentState = (box HookInternals.hooks.[setStateHookIndex]) :?> 't
 
-            HookInternals.currentHook <- HookInternals.currentHook + 1
-            currentState, setState
+        HookInternals.currentHook <- HookInternals.currentHook + 1
+        currentState, setState
